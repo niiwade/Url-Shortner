1bfc9ac98e77907a9c29fd224dfc1ad159a4264d
niiwade <niilantewade77@gmail.com>
2021-03-16 13:05:38 +0000

    debugging

---
+++ config/auth.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ensureAuthentcated: function(req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+
+        req.flash('error_msg', 'Please login to shorten  your urls');
+        res.redirect('/users/login');
+    }
+}
