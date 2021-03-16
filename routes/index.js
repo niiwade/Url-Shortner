a965d4d1f9391f8680fc354c5a10756041c756bd
niiwade <niilantewade77@gmail.com>
2021-03-16 12:57:27 +0000

    final push

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,15 @@
 
 const express = require('express');
 const router = express.Router();
+const { ensureAuthentcated } = require('../config/auth');
 
+// welcome page
 router.get('/', (req, res) => res.render('welcome'));
+
+//dashboard
+router.get('/dashboard', (req, res) =>
+    res.render('dashboard', {
+        name: req.user.name
+    }));
 
 module.exports = router;
